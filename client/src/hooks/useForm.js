2230fa293f4f55219a4bd69a9efb21d7678c51b5
Dominique K Kitchenakow <domikitchen@users.noreply.github.com>
2020-07-10 12:44:36 -0500

    completed mvp

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,5 +1,5 @@
 // write your custom hook here to control your checkout form
-import React, { useState } from "react";
+import { useState } from "react";
 
 const useForm = (bool, initialValue) => {
     const [showSuccessMessage, setShowSuccessMessage] = useState(bool);
